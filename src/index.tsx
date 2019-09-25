c318526583a6b417e95f770613bf2e421dcf7282
grahamcrackers <graham.rogers@servicemaster.com>
2019-09-25 08:40:08 -0500

    more folder cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './common/serviceWorker';
 
 import './index.css';
 import 'tachyons/css/tachyons.css';
