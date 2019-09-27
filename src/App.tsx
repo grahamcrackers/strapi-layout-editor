b480bbb57d052b00d88283a33b74124783d28b14
Graham Rogers <graham.rogers@servicemaster.com>
2019-09-27 17:43:03 -0500

    use craco to override react-scripts for postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'react-grid-layout/css/styles.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { PrivateRoute } from './common/private-route';
 import { Dashboard } from './components/dashboard';
@@ -9,9 +8,21 @@ import { Protected } from './pages/protected.page';
 import { Public } from './pages/public.page';
 import { BasicLayout } from './components/grid-layout';
 
+import stamp from './assets/img/stamp.svm.svg';
+
 const App: React.FC = () => {
     return (
         <Router>
+            <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+                <div className="flex-shrink-0">
+                    <img className="h-12 w-12" src={stamp} alt="ChitChat Logo" />
+                </div>
+                <div className="ml-6 pt-1">
+                    <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+                    <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
+                </div>
+            </div>
+
             <NavBar />
             <Route path="/public" component={Public} />
             <Route path="/login" component={LoginPage} />
