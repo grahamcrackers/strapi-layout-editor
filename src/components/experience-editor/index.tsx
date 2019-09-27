ed21ee05465210d81fd7f820f750883c879ea22c
Graham Rogers <graham.rogers@servicemaster.com>
2019-09-27 13:36:50 -0500

    get the right starting pos from strapi metadata

--- src/components/experience-editor/index.tsx
+++ src/components/experience-editor/index.tsx
@@ -4,40 +4,56 @@ import RGL, { WidthProvider } from 'react-grid-layout';
 import { useParams } from 'react-router-dom';
 import { useTraceUpdate } from '../../common/hooks/useTraceUpdate';
 import { StringifyLayout } from './stringify-layout';
-import { useContentData } from './useContentData';
+import { useContentData, ContentData } from './useContentData';
 
 import './example-styles.css';
 import './index.css';
+
+import { JsonStringify } from '../../common/json-stringify';
 
 const ReactGridLayout = WidthProvider(RGL);
 
 export const ExperienceEditor = props => {
     useTraceUpdate(props);
     const { contentType, itemId } = useParams();
-    const { data, layout, setLayout } = useContentData(contentType, itemId);
+    const { metadata, original, data, layout, setLayout } = useContentData(contentType, itemId);
 
     return (
         <section>
             <h1 className="mv0">{`${contentType}: ${itemId}`}</h1>
             <p className="lh-copy measure black-50 mt0">Experience Editor</p>
-
+            {/* List Elements and their values */}
+            {/* <ul className="list pl0">
+                {data.map(d => {
+                    return (
+                        <li key={d.key} className="pa3 pa4-ns bb b--black-10">
+                            <b className="db f3 mb1">{d.key}</b>
+                            <span className="f5 db lh-copy measure">{JSON.stringify(d.value)}</span>
+                        </li>
+                    );
+                })}
+            </ul> */}
             <StringifyLayout layout={layout} />
             <ReactGridLayout
                 layout={layout}
                 onLayoutChange={layouts => {
                     setLayout(layouts);
                 }}
-                cols={12}
-                rowHeight={30}
             >
-                {data.map(d => {
+                {data.map((d: ContentData) => {
                     return (
                         <div key={d.key}>
                             <span>{d.key}</span>
+                            <div>
+                                <pre>
+                                    <code>{JSON.stringify(d.value)}</code>
+                                </pre>
+                            </div>
                         </div>
                     );
                 })}
             </ReactGridLayout>
+
             {/* <JsonStringify {...data} /> */}
         </section>
     );
