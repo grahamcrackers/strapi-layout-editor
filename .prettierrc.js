7198f1fa79a827b095faf35f7e8fa25fb27496dd
Graham Rogers <graham.rogers@servicemaster.com>
2019-10-07 15:09:54 -0500

    remove comments

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,6 @@ module.exports = {
     semi: true,
     trailingComma: 'all',
     singleQuote: true,
-    // 80 is fine and what the prettier team suggests but... 
-    // adding types with typescript make the files start looking a bit wonky
     printWidth: 120,
     tabWidth: 4,
 };
