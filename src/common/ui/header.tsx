a4ae19e62c42b7432e1d59afde3020fdf0e619f6
grahamcrackers <graham.rogers@servicemaster.com>
2019-10-24 14:55:31 -0500

    keep header on top

--- src/common/ui/header.tsx
+++ src/common/ui/header.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
 
 export const Header: FC<React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>> = props => {
-    return <header className="fixed flex flex-row h-16 w-full shadow bg-white" {...props}></header>;
+    return <header className="fixed flex flex-row h-16 w-full shadow bg-white z-50" {...props}></header>;
 };
